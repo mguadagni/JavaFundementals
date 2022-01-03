46dac39bb11c326f9831e2be834b9b132268720d
Michael Guadagni <guadagnimichael@gmail.com>
2022-01-03 18:39:41 -0500

    Up to 02-03 of Mosh

--- src/com/careerdevs/Main.java
+++ src/com/careerdevs/Main.java
@@ -5,4 +5,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
+    byte age = 30;
+    long viewsCounts = 3_123_456_789L;
+    float price = 10.99F:
+    char letter = 'A';
+    boolean isEligible = false;
 }
