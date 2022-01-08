717a2100525c948aa8a6df290438e8cc422e3f59
Michael Guadagni <guadagnimichael@gmail.com>
2022-01-08 16:23:45 -0500

    Included notes/code up until A-02-08 "Arrays"

--- src/com/careerdevs/intro/DataTypes.java
+++ src/com/careerdevs/intro/DataTypes.java
@@ -1,33 +1,34 @@
 package com.careerdevs.intro;
 
 import java.awt.*;
+import java.util.Arrays;
 import java.util.Date;
 
 public class DataTypes {
     public static void main(String[] args) {
-        //01
+        //01 - intro
         int age = 30;
         int temperature = 20;
         System.out.println(age);
 
-        //02
+        //02 - variables
         int myAge = 30;
         int herAge = myAge;
         System.out.println(herAge);
 
-        //03
+        //03 - primitive
         byte age2 = 30;
         long viewsCount = 3_123_456_789L;
         float price = 10.99F;
         char letter = 'A';
         boolean isElligible = false;
 
-        //04
+        //04 - reference
         byte age3 = 30;
         Date now = new Date();
         System.out.println(now);
 
-        //05
+        //05 - prim vs reference
         byte x = 1;
         byte y = 1;
         x = 2;
@@ -37,7 +38,7 @@ public class DataTypes {
         point1.x = 2;
         System.out.println(point2);
 
-        //06
+        //06 - strings
         String message = "Hello World" + "!!";
         System.out.println(message);
         System.out.println(message.endsWith("!!"));
@@ -48,6 +49,24 @@ public class DataTypes {
                                 //.toUppercase()
                                 //.trim()
 
-        //07
+        //07 - escape sequences
+        String message2 = "Hello \"Mosh\"";
+        System.out.println(message2);
+        String message3 = "c:\\Windows\\...";
+        System.out.println(message3);
+        String message4 = "c:\nWindows\\...";
+        System.out.println(message4);
+        String message5 = "c:\tWindows\\...";
+        System.out.println(message5);
+
+        //08 - arrays
+        int[] numbers = new int[5];
+        numbers[0] = 1;
+        numbers[1] = 2;
+        System.out.println(Arrays.toString(numbers));
+        int[] numbers2 = {2, 3, 5, 1, 4};
+        System.out.println(numbers2.length);
+        Arrays.sort(numbers2);
+        System.out.println(Arrays.toString(numbers2));
     }
 }
