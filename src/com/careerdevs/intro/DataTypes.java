a8d6814d47c6353fc262a190fe0fa39655cd103a
mguadagni <guadagnimichael@gmail.com>
2022-02-19 19:03:52 -0500

    Completed up to A-02-14

--- src/com/careerdevs/intro/DataTypes.java
+++ src/com/careerdevs/intro/DataTypes.java
@@ -33,6 +33,7 @@ public class DataTypes {
         byte y = 1;
         x = 2;
         System.out.println(y);
+
         Point point1 = new Point(1,1);
         Point point2 = point1;
         point1.x = 2;
@@ -52,10 +53,13 @@ public class DataTypes {
         //07 - escape sequences
         String message2 = "Hello \"Mosh\"";
         System.out.println(message2);
+
         String message3 = "c:\\Windows\\...";
         System.out.println(message3);
+
         String message4 = "c:\nWindows\\...";
         System.out.println(message4);
+
         String message5 = "c:\tWindows\\...";
         System.out.println(message5);
 
@@ -64,9 +68,63 @@ public class DataTypes {
         numbers[0] = 1;
         numbers[1] = 2;
         System.out.println(Arrays.toString(numbers));
+
         int[] numbers2 = {2, 3, 5, 1, 4};
         System.out.println(numbers2.length);
+
         Arrays.sort(numbers2);
         System.out.println(Arrays.toString(numbers2));
+
+        //09 - Multi-dimensional Arrays
+        int[][] numbers3 = { {1, 2, 3}, {4, 5, 6} };
+        System.out.println(Arrays.deepToString(numbers3));
+
+        //10 - constants
+        final float PI = 3.14F; //cannot reassign variable
+
+        //11 - arithmetic expressions
+        double result = (double)10 / (double)3;
+        System.out.println(result);
+
+        //12 - order of operations
+        int x2 = 10 + 3 * 2;
+        System.out.println(x2);
+
+        int x3 = (10 + 3) * 2;
+        System.out.println(x3);
+
+        //13 - casting
+        short x4 = 1;
+        int y2 = x4 + 2;
+        System.out.println(y2);
+
+        double x5 = 1.1;
+        double y3 = x5 + 2;
+        System.out.println(y3);
+
+        double x6 = 1.1;
+        int y4 = (int)x6 + 2;
+        System.out.println(y4);
+
+        String x7 = "1";
+        int y5 = Integer.parseInt(x7) + 2;
+        System.out.println(y5);
+
+        //14 - the math class
+        int result2 = Math.round(1.1F);
+        System.out.println(result2);
+
+        int result3 = (int)Math.ceil(1.1F);
+        System.out.println(result3);
+
+        int result4 = (int)Math.floor(1.1F);
+        System.out.println(result4);
+
+        int result5 = Math.max(1, 2);
+        System.out.println(result5);
+
+        int result6 = (int) Math.round(Math.random() * 100);
+        System.out.println(result6);
+
     }
 }
